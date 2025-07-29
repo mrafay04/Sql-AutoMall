b2feb091eb3e204367ade3f6f45c124033094cd0
Rafay Raza <154420425+mrafay04@users.noreply.github.com>
2025-07-29 23:53:36 +0500

    Create Services Tables.sql

---
+++ Tables/Services Tables.sql
@@ -0,0 +1,25 @@
+create table MechanicalWork(
+Car_Name varchar(20),
+Car_Model int Not Null,
+Car_OwnerName varchar(20),
+Ciy_Name Varchar(20) default "Karachi",
+Total_PayedAmount Int Not null,
+Branch varchar(20) default "Karachi"
+);
+create table PPF(
+Car_Name varchar(20),
+Car_Model int Not Null,
+Car_OwnerName varchar(20),
+Ciy_Name Varchar(20) default "Karachi",
+Total_PayedAmount Int Not null,
+Branch varchar(20) default "Karachi"
+);
+
+-- create table DentingAndPainting(
+-- Car_Name varchar(20),
+-- Car_Model int Not Null,
+-- Car_OwnerName varchar(20),
+-- Ciy_Name Varchar(20) default "Karachi",
+-- Total_PayedAmount Int Not null,
+-- Branch varchar(20) default "Karachi"
+-- );
